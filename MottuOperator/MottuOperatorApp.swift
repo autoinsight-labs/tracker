1717892a94f96bc90b6d435b19a1884449c38d6e
Arthur Mariano <arthvm@proton.me>
2025-09-27 13:52:57 -0300

    Initial Commit

---
+++ MottuOperator/MottuOperatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MottuOperatorApp.swift
+//  MottuOperator
+//
+//  Created by Arthur Mariano on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MottuOperatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
